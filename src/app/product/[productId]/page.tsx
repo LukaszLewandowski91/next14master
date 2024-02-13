1651c373c9bd6dc6c2dfc9134b2e001b80932b66
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2024-02-13 20:12:42 +0100

    dodanie sugerowanych produktow

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,7 +1,21 @@
-export default function SingleProductPage({ params }: { params: { productId: string } }) {
+import { Suspense } from "react";
+import { getProductById } from "@/api/products";
+import { ProductCoverImage } from "@/ui/atoms/ProductCoverImage";
+import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescription";
+import { SuggestedProductsList } from "@/ui/organisms/SuggestedProductsList";
+
+export default async function SingleProductPage({ params }: { params: { productId: string } }) {
+	const product = await getProductById(params.productId);
 	return (
-		<div>
-			<h1>Product {params.productId}</h1>
-		</div>
+		<>
+			<article className="max-w-xs">
+				<ProductCoverImage {...product.coverImage} />
+				<ProductListItemDescription product={product} />
+			</article>
+			<Suspense fallback="Ładowanie...">
+				<h2>Related products</h2>
+				<SuggestedProductsList />
+			</Suspense>
+		</>
 	);
 }
