1ce269ed0e1856a8731a3cf0a15012ae4bc81edc
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2024-02-15 16:05:55 +0100

    add static page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,15 @@
 import { Suspense } from "react";
-import { getProductById } from "@/api/products";
+import { getProductById, getProductsList } from "@/api/products";
 import { ProductCoverImage } from "@/ui/atoms/ProductCoverImage";
 import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescription";
 import { SuggestedProductsList } from "@/ui/organisms/SuggestedProductsList";
+
+export const generateStaticParams = async () => {
+	const products = await getProductsList();
+	return products.map((product) => ({
+		productId: product.id,
+	}));
+};
 
 export default async function SingleProductPage({ params }: { params: { productId: string } }) {
 	const product = await getProductById(params.productId);
---
+++ src/app/products/[category]/[pageNumber]/page.tsx
@@ -0,0 +1,20 @@
+export const generateStaticParams = async ({ params }: { params: { category: string } }) => {
+	if (params.category === "t-shirts") {
+		return [{ pageNumber: "1" }, { pageNumber: "2" }];
+	} else if (params.category === "boots") {
+		return [{ pageNumber: "1" }, { pageNumber: "2" }, { pageNumber: "3" }];
+	}
+};
+export default function CategoryProductPage({
+	params,
+}: {
+	params: { category: string; pageNumber: string };
+}) {
+	return (
+		<div>
+			<h1>CategoryProductPage</h1>
+			<p>category: {params.category}</p>
+			<p>pageNumber: {params.pageNumber}</p>
+		</div>
+	);
+}
