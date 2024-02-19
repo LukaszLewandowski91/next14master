d3faded188b9908b5d64270ce50f43e46d8eb5f7
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2024-02-19 13:22:49 +0100

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-import { ActiveLink } from "@/ui/atoms/ActiveLink";
+import { Nav } from "@/ui/organisms/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,30 +17,7 @@ export default function RootLayout({
 	return (
 		<html lang="pl">
 			<body className={inter.className}>
-				<nav>
-					<ul className=" mt-2 flex justify-center space-x-4">
-						<li>
-							<ActiveLink
-								href="/"
-								className="text-blue-400 hover:text-blue-600"
-								activeClassName="text-blue-600 underline"
-								exact={true}
-							>
-								Home
-							</ActiveLink>
-						</li>
-						<li>
-							<ActiveLink
-								href="/products"
-								className="text-blue-400 hover:text-blue-600"
-								activeClassName="text-blue-600 underline"
-								exact={false}
-							>
-								All
-							</ActiveLink>
-						</li>
-					</ul>
-				</nav>
+				<Nav />
 				<section className="max-x-md mx-auto p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 					{children}
 				</section>
---
+++ src/ui/organisms/NavBar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import { ActiveLink } from "@/ui/atoms/ActiveLink";
+
+const NavLinks = [
+	{ href: "/", label: "Home", exact: true },
+	{ href: "/products", label: "All", exact: false },
+	// { href: "/categories/t-shirts", label: "T-shirts" },
+	// { href: "/categories/hoodies", label: "Hoodies" },
+	// { href: "/categories/accessories", label: "Accessories" },
+];
+
+export const Nav = () => {
+	const defaultClasses =
+		"flex h-full w-full min-w-[3rem] items-center justify-center border-b-2 px-1 pt-1 text-center text-sm font-medium text-slate-500";
+	const activeClasses = defaultClasses + " border-blue-500";
+	const classNames =
+		defaultClasses + " border-transparent hover:border-gray-300 hover:text-slate-700";
+	return (
+		<header className="sticky top-0 z-20 border-b bg-white bg-opacity-60 backdrop-blur-lg">
+			<div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+				<div className="flex flex-col justify-between gap-y-4 pb-4 lg:flex-row lg:items-center lg:pb-0">
+					<nav className="scrolling-touch scroll-shadows -mx-2 flex overflow-x-scroll lg:mx-0 lg:h-16 lg:overflow-x-auto">
+						<div className="hidden flex-shrink-0 items-center lg:flex"></div>
+						<ul className="flex h-16 max-w-full space-x-8 whitespace-nowrap lg:px-8">
+							{NavLinks.map((link) => (
+								<li key={link.href} className="first:pl-4 last:pr-4 lg:px-0">
+									<ActiveLink
+										href={link.href}
+										className={classNames}
+										activeClassName={activeClasses}
+										exact={link.exact}
+									>
+										{link.label}
+									</ActiveLink>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<div className="flex h-full flex-1 items-center px-2 lg:ml-6 lg:h-16 lg:justify-end">
+						<div className="ml-auto h-full lg:ml-4">
+							<Link
+								href="/cart"
+								className="flex h-full w-16 items-center justify-center border-b-2 border-transparent px-2 text-center text-sm font-medium text-slate-500 hover:border-gray-300 hover:text-slate-700"
+							>
+								<ShoppingCart className="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+								<div className="w-4">
+									<span className="ml-2 text-sm font-medium">0</span>
+									<span className="sr-only">items in cart, view bag</span>
+								</div>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
