ad69107100274b2bc5dcf33e989cd026f091084f
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2024-02-23 23:47:29 +0100

    add title page in collections

--- src/app/collections/[collection]/page.tsx
+++ src/app/collections/[collection]/page.tsx
@@ -1,6 +1,23 @@
 import { notFound } from "next/navigation";
 import { getProductsByCollectionSlug } from "@/api/products";
 import { ProductList } from "@/ui/organisms/ProductList";
+import { getCollectionsList } from "@/api/collections";
+
+export const generateMetadata = async ({ params }: { params: { collection: string } }) => {
+	const collections = await getCollectionsList();
+	if (!collections) {
+		throw notFound();
+	}
+	collections.filter((collection) => collection.slug === params.collection);
+	return {
+		title: `Kolekcja ${collections[0]?.name} - Sklep internetowy`,
+		description: `Produkty z kategorii ${collections[0]?.name}`,
+		openGraph: {
+			title: `Kategoria ${collections[0]?.name} - Sklep internetowy`,
+			description: `Produkty z kategorii ${collections[0]?.name}`,
+		},
+	};
+};
 
 export default async function CollectionPage({ params }: { params: { collection: string } }) {
 	const collection = await getProductsByCollectionSlug(params.collection);
