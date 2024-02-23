554d7ae2be784660ce4a80ce987c8f42cd019b82
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2024-02-23 23:37:54 +0100

    add search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import { getProductsList } from "@/api/products";
 import { ProductList } from "@/ui/organisms/ProductList";
+import { getCollectionsList } from "@/api/collections";
 
 export default async function HomePage() {
 	const products = await getProductsList(4, 0);
+	const collections = await getCollectionsList();
 
 	return (
 		<>
 			<ProductList products={products.data.slice(0, 4)} />
-			<Link href="/collections">Kolekcja</Link>
+
+			{collections.map((collection) => (
+				<Link href={`/collections/${collection.slug}`} key={collection.slug}>
+					{collection.name}
+				</Link>
+			))}
 		</>
 	);
 }
